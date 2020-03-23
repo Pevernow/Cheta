32b7203ce4da7769e5175f90c4590c2960ce076e
wzy2006 <3450354617@qq.com>
2020-03-23 15:30:38 +0800

    Support indexing and saving to JSON format file  --stable

--- js/main.js
+++ js/main.js
@@ -1,33 +1,27 @@
-$("document").ready(function(){});
+$("document").ready(function() {});
 //打开文件时调用此函数
 function openfile() {
   const reader = new FileReader();
   reader.readAsText(document.getElementById("open").files[0], "utf8"); // input.files[0]为第一个文件
   reader.onload = () => {
     data = JSON.parse(reader.result); // reader.result为获取结果
-    loadmenu(data.menu,document.getElementById("menu"))
-    
+    loadmenu(data.menu, document.getElementById("menu"));
   };
 }
 //当点击目录中的可编辑项目时调用此函数
 //作用：调用savearea保存旧数据，并把对应的docs刷新到编辑器主页
-//入参： docs json格式列表 每列都对应编辑器的一行文字
-//      t this 是当前目录项的指针，用于添加id以标记编辑项
-function flushdoc(docs,t){
+//入参： t this 是当前目录项的指针，用于添加id以标记编辑项
+function flushdoc(t) {
   savearea();
-  data = JSON.parse(docs);
+  var data = JSON.parse($(t).attr("docs"));
   $("#texts").empty();
-  for (i = 0, size = data.length; i < size; i++) {
-    $("#texts").append(
-      '<li class="text uk-text-break">' + data[i] + "</li>"
-    );
+  for (var i = 0, size = data.length; i < size; i++) {
+    $("#texts").append('<li class="text uk-text-break">' + data[i] + "</li>");
   }
-  try{
-  $("#editing").removeAttr('id')
-  }catch{
-    ;
-  }
-  $(t).attr('id','editing');
+  try {
+    $("#editing").removeAttr("id");
+  } catch {}
+  $(t).attr("id", "editing");
 }
 function inputdo() {
   $("body").click(function(e) {
@@ -94,26 +88,35 @@ function downFile(content, fileName) {
   URL.revokeObjectURL(url);
 }
 //保存当前编辑器部分数据到当前目录索引的位置
-function savearea(){
-  let out = [];
+function savearea() {
+  var out = [];
   $("#editor")
     .children(".uk-list")
     .children()
     .each(function() {
       out.push(this.innerText);
     });
-  $("#editing").attr('onclick',"flushdoc('"+JSON.stringify(out)+"',this)");
+  $("#editing").attr("docs", JSON.stringify(out));
+  $("#editing").attr("onclick", "flushdoc(this)");
 }
 //保存文件
 //（暂未实现）
+function eachmenu(docs, out) {
+  $(docs).children().each(function(i, dom) {
+    if ($(dom).children("a").length == 0) {
+      var childout=[dom.innerText.split(/[\s\n]/)[0]];
+      eachmenu($(dom).children()[0],childout);
+      out.push(childout);
+    }else{
+      var tmptexts=$(dom).attr("docs");
+      var tmpname=$(dom).children("a").text();
+      out.push({name: tmpname,data: tmptexts});
+    }
+  });
+}
 function savefile() {
-  let out = [];
-  $("#editor")
-    .children(".uk-list")
-    .children()
-    .each(function() {
-      out.push(this.innerText);
-    });
+  var out = {menu: ["main"]};
+  eachmenu($("#menu")[0],out.menu);
   downFile(JSON.stringify(out), "New.che");
 }
 //编辑点击的行
@@ -131,18 +134,30 @@ function editline(element) {
 //加载目录（递归按层级加载)
 //入参： menu 当前层对象
 //      doc 当前层对应dom对象
-function loadmenu(menu,doc) {
-  for (i=1,l=menu.length;i<l;i++) {
-    alert(Object.prototype.toString.call(menu[i]));
-    if(Object.prototype.toString.call(menu[i])=="[object Array]"){
-      if(menu[i][0]!="main"){
-        $(doc).append("<li>"+menu[i][0]+"</li>");
+function loadmenu(menu, doc) {
+  for (var i = 1, l = menu.length; i < l; i++) {
+    if (Object.prototype.toString.call(menu[i]) == "[object Array]") {
+      if (menu[i][0] != "main") {
+        $(doc).append(
+          $("<li>" + menu[i][0] + "</li>").append(
+            loadmenu(menu[i], document.createElement("ul"))
+          )
+        );
+      } else {
+        $(doc).append(loadmenu(menu[i], document.createElement("ul")));
       }
-      $(doc).append(loadmenu(menu[i],document.createElement("ul")));
-    }else{
-      $(doc).append("<li onclick=flushdoc(\""+menu[i]["data"]+"\",this)><a href='#'>"+menu[i]["name"]+"</a></li>");
+    } else {
+      $(doc).append(
+        '<li docs="' +
+          menu[i]["data"] +
+          "\" onclick=flushdoc(this)><a href='#'>" +
+          menu[i]["name"] +
+          "</a></li>"
+      );
     }
   }
-  $(doc).children().addClass("uk-margin-remove-top");
+  $(doc)
+    .children()
+    .addClass("uk-margin-remove-top");
   return doc;
 }
